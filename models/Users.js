a8551dd271d743f764ac21c88a70ab5d11b82e71
Nicholas Yang <nicho.yeo7@gmail.com>
2025-04-01 16:23:58 +0700

    🏷️ feat: add user model and schema

---
+++ models/Users.js
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    userFullName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    hashedPassword: {
+        type: String,
+        required: true,
+    },
+    totalExp: {
+        type: Number,
+        default: 0,
+    },
+    weeklyExp: {
+        type: Number,
+        default: 0,
+    },
+    dailyExp: {
+        type: Number,
+        default: 0,
+    },
+    totalStreak: {
+        type: Number,
+        default: 0,
+    },
+    highestStreak: {
+        type: Number,
+        default: 0,
+    },
+    totalGems: {
+        type: Number,
+        default: 0,
+    },
+    lastLogin: {
+        type: Date,
+        default: null,
+    },
+    profilePicture: {
+        type: String,
+        default: null,
+    },
+}, { timestamps: true });
+
+const User = mongoose.model("User", UserSchema);
+export default User;
