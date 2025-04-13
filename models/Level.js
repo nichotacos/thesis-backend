d72debe020af642b87e131cd85ef9cf544b25adc
Nicholas Yang <nicho.yeo7@gmail.com>
2025-04-13 17:43:25 +0700

    ✨ feat: add level schema and model

---
+++ models/Level.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const levelSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+}, { timestamps: true });
+
+const Level = mongoose.model("Level", levelSchema);
+
+export default Level;
