4ac3d64d105985bc5e66066dcedb672cf9e55734
Nicholas Yang <nicho.yeo7@gmail.com>
2025-04-17 22:15:11 +0700

    ✨ feat: add verify captcha function to register function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/Users.js";
 import bcrypt from "bcrypt";
+import { verifyCaptcha } from "../utils/verifyCaptcha.js";
 
 export async function fetchUsers(req, res) {
     try {
@@ -27,21 +28,28 @@ export async function storeUser(req, res) {
             return res.status(400).json({ message: "All fields are required" });
         }
 
-        const existingEmail = await User.findOne({ email });
-        if (existingEmail) {
-            return res.status(400).json({ message: "Email already exists" });
-        }
-
         const existingUsername = await User.findOne({ username });
         if (existingUsername) {
             return res.status(400).json({ message: "Username already exists" });
+        }
+
+        const existingEmail = await User.findOne({ email });
+        if (existingEmail) {
+            return res.status(400).json({ message: "Email already exists" });
         }
 
         if (password.length < 6) {
             return res.status(400).json({ message: "Password must be at least 6 characters long" });
         }
 
+        const isHuman = await verifyCaptcha(req.body.captchaToken);
+
+        if (!isHuman) {
+            return res.status(400).json({ message: "Verifikasi Captcha gagal!" });
+        }
+
         const hashedPassword = await bcrypt.hash(password, 10);
+
 
         const newUser = new User({
             username,
@@ -50,7 +58,7 @@ export async function storeUser(req, res) {
             hashedPassword,
         });
 
-        await newUser.save();
+        // await newUser.save();
         res.status(201).json({ message: "User created successfully", user: newUser });
     } catch (error) {
         res.status(500).json({ message: "Error storing user", error });
