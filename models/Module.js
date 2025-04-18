e0ef0a7af7458acc8bf43ce40474c92ffc5e6ff2
Nicholas Yang <nicho.yeo7@gmail.com>
2025-04-18 15:21:53 +0700

    🐛 fix: remove required on description

--- models/Module.js
+++ models/Module.js
@@ -7,7 +7,6 @@ const moduleSchema = new mongoose.Schema({
     },
     description: {
         type: String,
-        required: true,
     },
     level: {
         type: mongoose.Schema.Types.ObjectId,
