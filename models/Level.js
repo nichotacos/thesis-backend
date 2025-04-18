f229f119a10e882cf3091f116ae7c45621dc74f8
Nicholas Yang <nicho.yeo7@gmail.com>
2025-04-18 15:20:39 +0700

    ✨ feat: add more attribute

--- models/Level.js
+++ models/Level.js
@@ -5,6 +5,10 @@ const levelSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    actualBipaLevel: {
+        type: String,
+        required: true,
+    },
     description: {
         type: String,
         required: true,
