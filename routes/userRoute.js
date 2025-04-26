308e6bb1b12d6a09c23cf4c9f1491326b5971345
Nicholas Yang <nicho.yeo7@gmail.com>
2025-04-27 05:35:18 +0700

    ♻️ refactor: simple fix on import path

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { fetchUsers, storeUser } from "../controllers/userController.js"
+import { fetchUsers, storeUser } from "../controllers/UserController.js"
 
 const router = express.Router();
 
