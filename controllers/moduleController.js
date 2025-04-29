a74cb5bcf00e7f12220c9069dbc905a01f4d9462
Nicholas Yang <nicho.yeo7@gmail.com>
2025-04-29 22:00:16 +0700

    ♻️ refactor: remove populate for some attribute

--- controllers/moduleController.js
+++ controllers/moduleController.js
@@ -9,7 +9,7 @@ export async function getModules(req, res) {
             return res.status(400).json({ message: 'Please provide an array of levelIds.' });
         }
 
-        const modules = await Module.find({ level: { $in: levelIds } }).populate('level', 'name actualBipaLevel').lean();
+        const modules = await Module.find({ level: { $in: levelIds } }).populate('level').lean();
 
         res.status(200).json({
             message: 'Modules fetched successfully',
