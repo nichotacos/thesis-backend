0f63428d2672d1d0fdc33d4224f586b8e3ba0045
Nicholas Yang <nicho.yeo7@gmail.com>
2025-05-05 20:17:29 +0700

    ✨ feat: create achievement routes

--- routes/achievementsRoute.js
+++ routes/achievementsRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getAchievements, createAchievement } from '../controllers/achievementController.js';
+import { getAchievements, createAchievement, grantAchievement } from '../controllers/achievementController.js';
 
 const router = express.Router();
 
 router.get('/achievement', getAchievements);
 router.post('/achievement', createAchievement);
+router.post('/achievement/grant', grantAchievement);
 
 export default router;
