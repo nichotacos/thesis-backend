e0e6565257a34c106e0a8479c990e5765a1a2e63
Nicholas Yang <nicho.yeo7@gmail.com>
2025-05-13 22:14:31 +0700

    ✨ feat: add top 3 count attribute

--- models/Users.js
+++ models/Users.js
@@ -132,6 +132,10 @@ const UserSchema = new mongoose.Schema({
         type: Number,
         default: 0,
     },
+    top3Count: {
+        type: Number,
+        default: 0,
+    }
 }, { timestamps: true });
 
 const User = mongoose.model("User", UserSchema);
