94552eb48918af941e7a191ad49be050d73f2129
Nicholas Yang <nicho.yeo7@gmail.com>
2025-05-17 23:41:41 +0700

    ✨ feat: import upload middleware to route for image file uploading

--- routes/shopItemRoute.js
+++ routes/shopItemRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { createShopItem, getShop } from '../controllers/shopController.js';
+import upload from '../middlewares/uploadMiddleware.js';
 
 const router = express.Router();
 
 router.get('/shop-item', getShop);
-router.post('/shop-item', createShopItem);
+router.post('/shop-item', upload.single("image"), createShopItem);
 
 export default router;
