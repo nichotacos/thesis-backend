ac2e530760af326794f6ad200b04d4c171bcca42
Nicholas Yang <nicho.yeo7@gmail.com>
2025-06-01 19:14:10 +0700

    ✨ feat: able to upload image on creating achievement

--- routes/achievementsRoute.js
+++ routes/achievementsRoute.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import { getAchievements, createAchievement, grantAchievement } from '../controllers/achievementController.js';
 import authMiddleware from '../middlewares/authMiddleware.js';
+import upload from '../middlewares/uploadMiddleware.js';
 
 const router = express.Router();
 
 router.use(authMiddleware);
 
 router.get('/achievement', getAchievements);
-router.post('/achievement', createAchievement);
+router.post('/achievement', upload.single('badge'), createAchievement);
 router.post('/achievement/grant-achievement', grantAchievement);
 
 export default router;
