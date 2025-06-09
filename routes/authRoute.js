fd3f2742cb85f0aba3bcb2f0e09d5cd26d17c52a
Nicholas Yang <nicho.yeo7@gmail.com>
2025-06-09 17:56:02 +0700

    ✨ feat: move register route to auth route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { login, refreshAccessToken, logout } from '../controllers/authController.js';
+import { storeUser } from '../controllers/UserController.js';
 
 const router = express.Router();
 
 router.post('/login', login);
 router.post('/refresh-token', refreshAccessToken);
 router.post('/logout', logout);
+router.post('/register', storeUser);
 
 export default router;
